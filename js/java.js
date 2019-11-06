3e610d69dc4f0ef33131fad7db20e8eda9344c66
Cihan Karacaburun <2083040@talnet.nl>
2019-11-06 15:49:21 +0100

    Alles afgemaakt

--- js/java.js
+++ js/java.js
@@ -1,1 +1,10 @@
+function getMyDatum() {
+    vandaag = new Date();
+    dag = vandaag.getDate();
+    maand = vandaag.getMonth() + 1;
+    jaar = vandaag.getYear();
+    return dag + "-" + maand + "-" + jaar;
+}
+
+
 
